dda08eb55ab81cb700a96a119d0c734cc7ac3740
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-05-15 17:32:32 +0400

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import ErrorPage from './assets/pages/error-page'
-import HomePage from './assets/pages/home-page'
-import HeaderComp from './assets/layout/header'
-import FooterComp from './assets/layout/footer'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ErrorPage from "./assets/pages/error-page";
+import HomePage from "./assets/pages/home-page";
+import HeaderComp from "./assets/layout/header";
+import FooterComp from "./assets/layout/footer";
 
 function App() {
-
   return (
     <div className="App">
       <HeaderComp />
       <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-        <Route path='*' element={<ErrorPage />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
       <FooterComp />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/assets/layout/header/index.jsx
+++ src/assets/layout/header/index.jsx
@@ -1,49 +1,60 @@
-import React, { useState } from 'react'
-import logo from '../images/Shortly.svg'
-import { NavLink } from 'react-router-dom'
-import { Card } from 'antd';
-import './index.scss'
+import React, { useState } from "react";
+import logo from "../images/Shortly.svg";
+import { NavLink } from "react-router-dom";
+import { Card } from "antd";
+import "./index.scss";
 
 const HeaderComp = () => {
-    const [Toggle, setToggle] = useState(false)
+  const [Toggle, setToggle] = useState(false);
 
-    return (
-        <div className='header'>
-            <div className="logo">
-                <img src={logo} alt="" />
-                <div className="navigation">
-                    <NavLink to={'/45412'}>Features</NavLink>
-                    <NavLink to={'/asdas'}>Pricing</NavLink>
-                    <NavLink to={'/sasf'}>Resources</NavLink>
-                </div>
-            </div>
-
-            {Toggle && (
-                <div className="site-card-border-less-wrapper">
-                    <Card title={
-                        <>
-                            Features <br />
-                            Pricing <br />
-                            Resources
-                        </>
-                    } bordered={false} style={{ width: 250, backgroundColor: '#3A3054' }}>
-                        <p>Login</p>
-                        <button>Sign UP</button>
-                    </Card>
-                </div>
-            )}
-
-            <div className="headerRight">
-                <div className="menuIcon">
-                    <span><i className="fa-solid fa-bars" onClick={() => {
-                        setToggle(!Toggle)
-                    }}></i></span>
-                </div>
-                <NavLink to={'/asds'}>Login</NavLink>
-                <button><NavLink to={'/sds'}>Sign Up</NavLink></button>
-            </div>
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="Shortly" />
+        <div className="navigation">
+          <NavLink to={"/features"}>Features</NavLink>
+          <NavLink to={"/pricing"}>Pricing</NavLink>
+          <NavLink to={"/resources"}>Resources</NavLink>
         </div>
-    )
-}
+      </div>
 
-export default HeaderComp
+      {Toggle && (
+        <div className="site-card-border-less-wrapper">
+          <Card
+            title={
+              <>
+                Features <br />
+                Pricing <br />
+                Resources
+              </>
+            }
+            bordered={false}
+            style={{ width: 250, backgroundColor: "#3A3054" }}
+          >
+            <p>Login</p>
+            <button>Sign UP</button>
+          </Card>
+        </div>
+      )}
+
+      <div className="headerRight">
+        <div className="menuIcon">
+          <span>
+            <i
+              className="fa-solid fa-bars"
+              onClick={() => {
+                setToggle(!Toggle);
+              }}
+            ></i>
+          </span>
+        </div>
+        <NavLink to={"/login"}>Login</NavLink>
+        <button>
+          <NavLink to={"/signUp"}>Sign Up</NavLink>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderComp;
